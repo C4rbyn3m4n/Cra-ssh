dd4c0eadc769b820335fa2940ad0bc20c8bd816f
Ezequiel Juarez Garcia <ezequieljuarezg@gmail.com>
2017-02-17 09:18:58 -0500

    Add another comment to script 02 in opencv testing

--- opencv_testing/02_using_matplotlib.py
+++ opencv_testing/02_using_matplotlib.py
@@ -15,7 +15,9 @@ filename = 'saitama_chibi.png'
 # img = cv2.imread(filename,0)    # display in grayscale
 img = cv2.imread(filename,1)[:,:,::-1] # display in color
 
-# I don't know what cmap and interpolation options do.
+# I don't know what cmap and interpolation options do. Using just
+# interpolation, the image looks less jagged and softer around the
+# edges.
 plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
 plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
 plt.show()
