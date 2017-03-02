7be529e9a6edd0a8db13e849ad36b37d267a4095
Ezequiel Juarez Garcia <ezequieljuarezg@gmail.com>
2017-03-01 20:35:22 -0500

    Push code for blob tracking that I found online

--- OpenCV/03_video_capture.py
+++ OpenCV/03_video_capture.py
@@ -13,7 +13,8 @@ while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # smurf_filter = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     # Display the resulting frame. To display in color, comment
     # out the line above 'gray = ...' and change the word 'gray'
     # below to 'frame'
